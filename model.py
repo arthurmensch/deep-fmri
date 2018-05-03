27219652bce13879516a6196e00b54d9069aa337
MENSCH Arthur <arthur.mensch@inria.fr>
2018-05-03 10:17:08 +0200

    Gaussian KL

--- model.py
+++ model.py
@@ -13,34 +13,38 @@ class Encoder(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv3d(1, 16, 3, 2, 1),
             nn.ReLU(),
+            nn.BatchNorm3d(16),
             nn.Conv3d(16, 16, 3, 1, 1),
             nn.ReLU(),
             nn.BatchNorm3d(16),
 
             nn.Conv3d(16, 32, 3, 2, 1),
             nn.ReLU(),
+            nn.BatchNorm3d(32),
             nn.Conv3d(32, 32, 3, 1, 1),
             nn.ReLU(),
             nn.BatchNorm3d(32),
 
             nn.Conv3d(32, 64, 3, 2, 1),
             nn.ReLU(),
+            nn.BatchNorm3d(64),
             nn.Conv3d(64, 64, 3, 1, 1),
             nn.ReLU(),
             nn.BatchNorm3d(64),
 
             nn.Conv3d(64, 128, 3, 2, 1),
             nn.ReLU(),
+            nn.BatchNorm3d(128),
             nn.Conv3d(128, 128, 3, 1, 1),
             nn.ReLU(),
             nn.BatchNorm3d(128),
 
             nn.Conv3d(128, 256, 3, 2, 1),
             nn.ReLU(),
+            nn.BatchNorm3d(256),
             nn.Conv3d(256, 256, 3, 1, 1),
             nn.ReLU(),
             nn.BatchNorm3d(256),
-
         )
 
         self.dense = nn.Linear(256, embedding_size)
@@ -51,8 +55,9 @@ class Encoder(nn.Module):
         img = self.pad(img)
         conv_img = self.conv(img)
         avg_channel = conv_img.view(batch_size, 256, -1).mean(dim=2)
-        mean = self.dense(F.dropout(avg_channel, p=0.1))
-        log_std = self.dense_var(F.dropout(avg_channel, p=0.1))
+        avg_channel = F.dropout(avg_channel, p=0.1)
+        mean = self.dense(avg_channel)
+        log_std = self.dense_var(avg_channel)
         return mean, log_std
 
 
@@ -63,51 +68,55 @@ class Decoder(nn.Module):
         self.dense = nn.Linear(embedding_size, 256)
 
         self.deconv = nn.Sequential(
-            nn.BatchNorm3d(256),
             nn.ConvTranspose3d(in_channels=256, out_channels=256,
                                kernel_size=3, stride=1, padding=1,
                                output_padding=0, dilation=1),
             nn.ReLU(),
+            nn.BatchNorm3d(256),
             nn.ConvTranspose3d(in_channels=256, out_channels=128,
                                kernel_size=3, stride=2, padding=1,
                                output_padding=1, dilation=1),
             nn.ReLU(),
-
             nn.BatchNorm3d(128),
+
             nn.ConvTranspose3d(in_channels=128, out_channels=128,
                                kernel_size=3, stride=1, padding=1,
                                output_padding=0, dilation=1),
+            nn.BatchNorm3d(128),
             nn.ReLU(),
             nn.ConvTranspose3d(in_channels=128, out_channels=64,
                                kernel_size=3, stride=2, padding=1,
                                output_padding=1, dilation=1),
             nn.ReLU(),
-
             nn.BatchNorm3d(64),
+
             nn.ConvTranspose3d(in_channels=64, out_channels=64,
                                kernel_size=3, stride=1, padding=1,
                                output_padding=0, dilation=1),
             nn.ReLU(),
+            nn.BatchNorm3d(64),
             nn.ConvTranspose3d(in_channels=64, out_channels=32,
                                kernel_size=3, stride=2, padding=1,
                                output_padding=1, dilation=1),
             nn.ReLU(),
-
             nn.BatchNorm3d(32),
+
             nn.ConvTranspose3d(in_channels=32, out_channels=32,
                                kernel_size=3, stride=1, padding=1,
                                output_padding=0, dilation=1),
             nn.ReLU(),
+            nn.BatchNorm3d(32),
             nn.ConvTranspose3d(in_channels=32, out_channels=16,
                                kernel_size=3, stride=2, padding=1,
                                output_padding=1, dilation=1),
             nn.ReLU(),
-
             nn.BatchNorm3d(16),
+
             nn.ConvTranspose3d(in_channels=16, out_channels=16,
                                kernel_size=3, stride=1, padding=1,
                                output_padding=0, dilation=1),
             nn.ReLU(),
+            nn.BatchNorm3d(16),
             nn.ConvTranspose3d(in_channels=16, out_channels=1,
                                kernel_size=3, stride=2, padding=1,
                                output_padding=1, dilation=1),
@@ -147,5 +156,5 @@ class VAE(nn.Module):
 
 
 def gaussian_kl(mean, log_std):
-    return .5 * torch.mean(mean ** 2 + torch.exp(log_std * 2)
-                           - log_std - 1)
+    return .5 * torch.sum(mean ** 2 + torch.exp(log_std * 2)
+                          - log_std - 1) / mean.shape[0]
--- train.py
+++ train.py
@@ -5,11 +5,12 @@ import torch.nn.functional as F
 from os.path import expanduser
 from torch.optim import Adam
 from torch.utils.data import DataLoader
+from torchsummary import summary
 
 from data import get_dataset
 from model import VAE
 
-train_dataset, test_dataset = get_dataset(in_memory=True)
+train_dataset, test_dataset = get_dataset(in_memory=False)
 
 train_loader = DataLoader(train_dataset, batch_size=32,
                           shuffle=True)
@@ -18,61 +19,61 @@ test_loader = DataLoader(test_dataset, batch_size=32,
 model = VAE()
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 model = model.to(device)
-# print(summary(model, (1, 91, 109, 91)))
-
-optimizer = Adam(model.parameters(), amsgrad=True)
-
-n_epochs = 100
-total_loss = 0
-
-n_batch = math.ceil(len(train_dataset) / 32)
-
-for epoch in range(n_epochs):
-    epoch_batch = 0
-    verbose_loss = 0
-    verbose_penalty = 0
-    verbose_batch = 0
-    for this_data in train_loader:
-        this_data = this_data.to(device)
-        model.zero_grad()
-        rec, penalty = model(this_data)
-        loss = F.mse_loss(rec, this_data)
-        loss += penalty
-        loss.backward()
-        optimizer.step()
-        verbose_loss += loss.item()
-        verbose_penalty += penalty.item()
-        epoch_batch += 1
-        verbose_batch += 1
-        if epoch_batch % 10 == 0:
-            with torch.no_grad():
-                val_batch = 0
-                val_loss = 0
-                val_penalty = 0
-                for this_test_data in test_loader:
-                    this_test_data = this_test_data.to(device)
-                    rec, this_val_penalty = model(this_test_data)
-                    this_val_loss = F.mse_loss(rec, this_test_data)
-                    val_loss += this_val_loss.item()
-                    val_penalty += this_val_penalty.item()
-                    val_batch += 1
-                val_loss /= val_batch
-                val_penalty /= val_batch
-
-            verbose_loss /= verbose_batch
-            verbose_penalty /= verbose_batch
-            print('Epoch %i, batch %i/%i,'
-                  'train_objective: %.4e,'
-                  'train_penalty: %.4f,'
-                  'val_objective: %.4e,'
-                  'val_penalty: %.4f' % (epoch, epoch_batch,
-                                         n_batch,
-                                         verbose_loss, verbose_penalty,
-                                         val_loss, val_penalty
-                                         ))
-            verbose_batch = 0
-            train_loss = 0
-            penalty = 0
-    state_dict = model.state_dict()
-    name = 'vae_e_%i_loss_%.4e.pkl' % (epoch, verbose_loss)
-    torch.save(state_dict, expanduser('~/output/deep-fmri/%s' % name))
+print(summary(model, (1, 91, 109, 91)))
+#
+# optimizer = Adam(model.parameters(), amsgrad=True)
+#
+# n_epochs = 100
+# total_loss = 0
+#
+# n_batch = math.ceil(len(train_dataset) / 32)
+#
+# for epoch in range(n_epochs):
+#     epoch_batch = 0
+#     verbose_loss = 0
+#     verbose_penalty = 0
+#     verbose_batch = 0
+#     for this_data in train_loader:
+#         this_data = this_data.to(device)
+#         model.zero_grad()
+#         rec, penalty = model(this_data)
+#         loss = F.mse_loss(rec, this_data)
+#         loss += penalty
+#         loss.backward()
+#         optimizer.step()
+#         verbose_loss += loss.item()
+#         verbose_penalty += penalty.item()
+#         epoch_batch += 1
+#         verbose_batch += 1
+#         if epoch_batch % 10 == 0:
+#             with torch.no_grad():
+#                 val_batch = 0
+#                 val_loss = 0
+#                 val_penalty = 0
+#                 for this_test_data in test_loader:
+#                     this_test_data = this_test_data.to(device)
+#                     rec, this_val_penalty = model(this_test_data)
+#                     this_val_loss = F.mse_loss(rec, this_test_data)
+#                     val_loss += this_val_loss.item()
+#                     val_penalty += this_val_penalty.item()
+#                     val_batch += 1
+#                 val_loss /= val_batch
+#                 val_penalty /= val_batch
+#
+#             verbose_loss /= verbose_batch
+#             verbose_penalty /= verbose_batch
+#             print('Epoch %i, batch %i/%i,'
+#                   'train_objective: %.4e,'
+#                   'train_penalty: %.4f,'
+#                   'val_objective: %.4e,'
+#                   'val_penalty: %.4f' % (epoch, epoch_batch,
+#                                          n_batch,
+#                                          verbose_loss, verbose_penalty,
+#                                          val_loss, val_penalty
+#                                          ))
+#             verbose_batch = 0
+#             train_loss = 0
+#             penalty = 0
+#     state_dict = model.state_dict()
+#     name = 'vae_e_%i_loss_%.4e.pkl' % (epoch, verbose_loss)
+#     torch.save(state_dict, expanduser('~/output/deep-fmri/%s' % name))
